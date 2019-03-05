33f79d9c3f7812bcd0724d8c7fdd6b0efadf289f
bbvch13531 <bbvch13531@gmail.com>
2019-03-05 23:34:57 +0900

    FEAT: Add stack header
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_score/test/stack.h
+++ ssu_score/test/stack.h
@@ -1,8 +1,34 @@
 #ifndef STACK_H_
 
+#include <stdbool.h>
 #define MAX 100000
-typedef struct {
 
+typedef struct stack {
+    char* arr[MAX];
+    int size;
 } stack;
+
+void init(stack *s){
+    s->size = 0;
+}
+
+void pop(stack *s){
+    s->size--;
+    s->arr[s->size] = '\0';        
+}
+
+void push(stack *s, char* c){
+    s->arr[s->size] = c;
+    s->size++;
+};
+
+char* top(stack *s){
+    return s->arr[s->size-1];
+}
+
+bool empty(stack *s){
+    if(s->size) return false;
+    else return true;
+}
 
 #endif // STACK_H_
