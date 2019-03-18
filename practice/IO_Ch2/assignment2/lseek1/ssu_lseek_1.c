4a31aa5c747ed39768141d80904d4e99b129fbea
bbvch13531 <bbvch13531@gmail.com>
2019-03-18 19:06:27 +0900

    FEAT: lseek1
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/IO_Ch2/assignment2/lseek1/ssu_lseek_1.c
+++ practice/IO_Ch2/assignment2/lseek1/ssu_lseek_1.c
@@ -2,24 +2,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys.types.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#define SEC_TO_MICRO 1000000
 
 int main(int argc, char** argv){
-	char *fname = "ssu_text.txt";
+	struct timeval begin_t, end_t;
+
+	char *fname = "ssu_test.txt";
 	off_t fsize;
 	int fd;
-
+	gettimeofday(&begin_t, NULL);
 	if((fd = open(fname, O_RDONLY)) < 0){
 		fprintf(stderr, "Open error for %s\n",fname);
 		exit(1);
 	}
 
 	if((fsize = lseek(fd, 0, SEEK_END)) < 0){
-		fpirntf(stderr, "lseek error\n");
+		fprintf(stderr, "lseek error\n");
 		exit(1);
 	}
 
-	printf("The size of <%s> is $ld bytes.\n", fname, fsize);
+	printf("The size of <%s> is %ld bytes.\n", fname, fsize);
+	gettimeofday(&end_t, NULL);
 
+	end_t.tv_sec -= begin_t.tv_sec;
+	if(end_t.tv_usec < begin_t.tv_usec){
+		end_t.tv_sec--;
+		end_t.tv_usec += SEC_TO_MICRO;
+	}
+	end_t.tv_usec -= begin_t.tv_usec;
+	printf("Runtime: %ld:%ld\n",end_t.tv_sec, end_t.tv_usec);
 	exit(0);
 }
