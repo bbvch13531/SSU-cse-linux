d55371e03cce9915115a29184056a882c67b0df1
bbvch13531 <bbvch13531@gmail.com>
2019-03-18 19:06:50 +0900

    FEAT: lseek2
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/IO_Ch2/assignment2/lseek2/ssu_lseek_2.c
+++ practice/IO_Ch2/assignment2/lseek2/ssu_lseek_2.c
@@ -3,16 +3,20 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/time.h>
 
+#define SEC_TO_MICRO 1000000
 #define CREAT_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
 char buf1[] = "1234567890";
 char buf2[] = "ABCDEFGHIJ";
 
 int main(int argc, char** argv){
+	struct timeval begin_t, end_t;
 	char *fname = "ssu_hole.txt";
 	int fd;
 
+	gettimeofday(&begin_t, NULL);
 	if((fd = creat(fname, CREAT_MODE)) < 0){
 		fprintf(stderr, "Creat error for %s\n",fname);
 		exit(1);
@@ -30,6 +34,14 @@ int main(int argc, char** argv){
 		fprintf(stderr, "buf2 write error\n");
 		exit(1);
 	}
+	gettimeofday(&end_t, NULL);
 
+	end_t.tv_sec -= begin_t.tv_sec;
+	if(end_t.tv_usec < begin_t.tv_usec){
+		end_t.tv_sec--;
+		end_t.tv_usec += SEC_TO_MICRO;
+	}
+	end_t.tv_usec -= begin_t.tv_usec;
+	printf("Runtime: %ld:%ld\n",end_t.tv_sec, end_t.tv_usec);
 	exit(0);
 }
