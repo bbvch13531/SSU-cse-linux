fb61839133714dfdf527a1964eb802f476d2f1e7
bbvch13531 <bbvch13531@gmail.com>
2019-03-25 14:47:58 +0900

    FEAT: unamk function
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ practice/FD_Ch3/assignment5/umask/ssu_umask.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "calTime.h"
+
+#define RW_MODE
+(S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+
+int main(void){
+    struct timeval begin_t, end_t;
+    char *fname1 = "ssu_file1";
+    char *fname2 = "ssu_file2";
+
+    gettimeofday(&begin_t, NULL);
+    umask(0);
+
+    if(creat(fname1, RW_MODE) < 0){
+        fprintf(stderr, "creat error for %s\n",fname1);
+        exit(1);        
+    }
+    // 0666
+    umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+
+    if(creat(fname2, RW_MODE) < 0){
+        fprintf(stderr, "creat error for %s\n",fname2);
+        exit(1);     
+    }
+    gettimeofday(&end_t, NULL);
+    ssu_runtime(&begin_t, &end_t);
+    exit(0);
+}
