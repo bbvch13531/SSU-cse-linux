e0095035d2fac43b9c2f332e23bd7e488a770037
bbvch13531 <bbvch13531@gmail.com>
2019-03-28 01:32:27 +0900

    FEAT: Read std files
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_score/ssu_score.c
+++ ssu_score/ssu_score.c
@@ -12,15 +12,15 @@
 
 void showHelp(void);
 int checkScoreTable(char *pathname);
-char* readFiles(char *pathname);
 int selectType(void);
 int* selectProblemPoint(int scoreType);
 void readANS(char *pathname);
+void readSTD(char *pathname);
 
 int strToNum(char *name);
 
 int fd_ans[110], fd_std[110][110]; // ANS/files, STD/num/files의 fd를 저장
-char ans_files[110][200], std_files[110][200];
+char stdDirname[110][200];
 int problemNum = 0, studentNum = 0; // number of problem and students
 
 typedef struct {
@@ -29,7 +29,13 @@ typedef struct {
     int id;
 } AnsFile;
 
+typedef struct {
+    char stdName[NAME_LEN];
+    AnsFile file[110];
+} StdFile;
+
 AnsFile ansFile[110];
+StdFile stdFile[110];
 
 int main(int argc, char** argv){
     int opt;
@@ -94,6 +100,8 @@ int main(int argc, char** argv){
 
     //  score_table.csv 파일이 있는지 확인. 없으면 생성 
     csvFileDes = checkScoreTable(answer_dir);
+    
+    /*// test debug
     // ANS_DIR 읽고 어떤 문제가 있는지 저장 (.txt, .c 구분)
     readANS(answer_dir);
     for(int i=0; i<problemNum; i++){
@@ -117,8 +125,18 @@ int main(int argc, char** argv){
     for(int i=0; i<problemNum; i++){
         printf("%s %d\n",ansFile[i].name, ansFile[i].id);
     }
-    
-    
+    //*/
+    readANS(answer_dir);
+    readSTD(student_dir);
+    for(int i=0; i<studentNum; i++){
+        printf("%s\n",stdFile[i].stdName);
+        for(int j=0; j<10; j++){
+            printf("%s\n",stdFile[i].file[j].name);
+        }
+    }
+    // for(int j=0; j<problemNum; j++){
+    //     printf("%s %s\n",stdFile[1].stdName, stdFile[0].file[j].name);
+    // }
     // filenames = readFiles(answer_dir);
     scoreType = selectType();
     scorePoints = selectProblemPoint(scoreType);
@@ -161,22 +179,6 @@ int checkScoreTable(char *pathname){
     }
 
     return fd;
-}
-
-/*
-    Read files in pathname and verify type of problems
-*/
-char* readFiles(char *pathname){
-    struct dirent *dentry;
-    struct stat statbuf;
-    char filenames[110];
-    char curdir[1000];
-    DIR *dirp;
-
-    if((dirp = opendir(pathname)) == NULL || chdir(pathname) == -1){
-        fprintf(stderr, "opendir : chdir error");
-        exit(1);
-    }
 }
 
 int selectType(){
@@ -266,8 +268,7 @@ void readANS(char *pathname){
                     //if(strstr(filename2, ".txt")){
                         fd_ans[i] = open(dentry2->d_name,O_RDONLY);
                         ansFile[i].fd = fd_ans[i];
-
-                        memcpy(ans_files[i], dentry2->d_name, NAME_LEN);                                        
+                                       
                         memcpy(ansFile[i].name, dentry2->d_name, NAME_LEN);
                         
                         // strToNum debugging...
@@ -290,6 +291,104 @@ void readANS(char *pathname){
         }
     }
     problemNum = i;
+    chdir("..");
+}
+
+void readSTD(char *pathname){
+    struct dirent *dentry1, *dentry2;
+    struct stat statbuf1, statbuf2;
+    char filename1[200], filename2[200];
+    char curdir[200], buf[1024];
+    int i = 0, j = 0;
+    DIR *dirp1, *dirp2;
+
+    if((dirp1 = opendir(pathname)) == NULL || chdir(pathname) == -1){
+        fprintf(stderr, "opendir: chdir error for %s\n",pathname);
+        exit(1);
+    }
+
+    while((dentry1 = readdir(dirp1)) != NULL){
+        if(dentry1->d_ino == 0)
+            continue;
+        memcpy(filename1, dentry1->d_name, NAME_LEN);
+        if(strcmp(filename1, "score_table.csv") == 0) continue;
+        memcpy(stdDirname[i], filename1, NAME_LEN);
+        memcpy(stdFile[i].stdName, stdDirname[i], NAME_LEN);
+        // printf("%s\n",filename1);
+        if(stat(filename1, &statbuf1) == -1){
+            fprintf(stderr, "stat error for %s\n", filename1);
+            perror("stat error: ");
+            exit(1);
+        }
+
+        if(S_ISDIR(statbuf1.st_mode)){  // if directory
+            if(strncmp(".", filename1, 1) == 0 || strncmp("..", filename1, 2) == 0)
+                continue;
+            
+            if((dirp2 = opendir(filename1)) == NULL){
+                fprintf(stderr, "opendir error for %s\n", filename2);
+                exit(1);
+            }
+            
+            chdir(filename1);
+            // printf("%15s\n",filename1);
+            j=0;
+            while((dentry2 = readdir(dirp2)) != NULL){
+                if(dentry2->d_ino == 0)
+                    continue;
+                memcpy(filename2, dentry2->d_name, NAME_LEN); 
+
+                if(strncmp(".", filename2, 1) == 0 || strncmp("..", filename2, 2) == 0)
+                    continue;
+                // printf("%s\n",filename2);
+                
+                if(stat(filename2, &statbuf2) == -1){
+                    fprintf(stderr, "stat error for %s\n", filename2);
+                    exit(1);
+                }
+
+                if(S_ISREG(statbuf2.st_mode)){
+                    // printf("%s\n",filename2);
+                    //if(strstr(filename2, ".txt")){
+                        fd_std[i][j] = open(dentry2->d_name,O_RDONLY);
+                        stdFile[i].file[j].fd = fd_std[i][j];
+                        // printf("%d\n",stdFile[i].file[j].fd);
+                        // printf("%s\n",dentry2->d_name );
+                        memcpy(stdFile[i].file[j].name, dentry2->d_name, NAME_LEN);
+                        
+                        // strToNum
+                        stdFile[i].file[j].id = strToNum(stdFile[i].file[j].name);
+                        // printf("%s %d\n",stdFile[i].file[j].name, stdFile[i].file[j].id);
+                        // while(read(fd_std[i][j], buf, 1024)){
+                        //     printf("%s\n",buf);
+                        // }
+                        
+                    //}
+                    /* .c file 
+                    else{
+
+                    }
+                    */
+                }
+                j++;
+                printf("i = %d, j = %d\n",i,j);
+            }
+            
+            
+            chdir("..");
+            if(dentry1->d_ino == 0) continue;
+            i++;
+        }
+    }   
+    
+    studentNum = i;
+
+    // for(int i=0; i<studentNum; i++){
+    //     printf("%s\n",stdFile[i].stdName);
+    //     for(int j=0; j<10; j++){
+    //         printf("%s\n",stdFile[i].file[j].name);
+    //     }
+    // }
 }
 
 int strToNum(char *name){
