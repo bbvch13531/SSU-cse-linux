f5d1a7fb04d79097e9202acf932e14b3fdb53f72
bbvch13531 <bbvch13531@gmail.com>
2019-04-01 03:21:28 +0900

    FEAT: Open .stdout files
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_score/ssu_score.c
+++ ssu_score/ssu_score.c
@@ -51,7 +51,10 @@ void runAnsProgram(char *pathname);
 void printANSProgram(char *pathname);
 void printSTDProgram(char *pathname);
 
-int compareResult(char *pathname);
+// compare
+int compareResult(char *pathname1, char *pathname2);
+int matchStdout(char *pathname1, char *pathname2);
+int compareFiles(int fd1, int fd2);
 // thread
 void *watchdog(void *arg);
 void *workerThread(void *arg);
@@ -176,7 +179,7 @@ int main(int argc, char** argv){
     //     }
     // }
 
-    runAnsProgram(answer_dir);
+    // runAnsProgram(answer_dir);
     printf("runAnsProgram end!\n");
     // runStdProgram(student_dir);
 
@@ -205,7 +208,7 @@ int main(int argc, char** argv){
     
     // // ANS와 STD를 비교
     
-    // compareResult(student_dir);
+    compareResult(answer_dir, student_dir);
 
 
     
@@ -614,20 +617,26 @@ int strToNum(char *name){
 void runStdProgram(char *pathname){
     struct dirent **namelist1, **namelist2;
     struct stat statbuf1, statbuf2;
+    char filename1[NAME_LEN], filename2[NAME_LEN], filename3[NAME_LEN];
     sigset_t sigset;
-    char cmd[50], buf1[50], buf2[50], problemName[50];
-    char fileerror[50];
+    char cmd[2048], problemName[NAME_LEN];
+    char fileerror[NAME_LEN];
     int fd,len, fd_err;
     off_t errorSize;
-
-    system("pwd");
     
-    mkdir("STD",0644);
-    chdir(pathname);
-
     // setup main thread signal
 
     signal(SIGALRM, sig_handler);
+    
+    umask(0);
+    mkdir("STD",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    // mkdir("ANS",775);
+    chdir("STD");
+
+    printf("runStdProgram\n---------------------\n");
+    system("pwd");
+
+
     // sigemptyset(&sigset);
     // sigaddset(&sigset, SIGALRM);
     // sigprocmask(SIG_BLOCK, &sigset, NULL);
@@ -635,48 +644,35 @@ void runStdProgram(char *pathname){
     // pthread_sigmask(SIG_BLOCK, &sigset, NULL);
 
     // pthread_create(&wtid, NULL, (void *)watchdog, NULL);
-
+    
     for(int i=0; i<studentNum; i++){
+        strcpy(filename1, stdFile[i].stdName);
+        if(strcmp(filename1, "") == 0)
+                continue;
+        mkdir(filename1, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);  
+
+        chdir(filename1);
+
         for(int j=0; j<problemNum; j++){
-            if(stdFile[i].file[j].type ==2){
-                printf("runStdProgram in %s %s\n", stdFile[i].stdName, stdFile[i].file[j].name);
-
-               
-                memset(buf1, 0, 50);
-                memset(buf2, 0, 50);
-                memset(cmd, 0, 50);
+            strcpy(filename2,  ansFile[j].name);
+            if(strcmp(filename2, "") == 0)
+                continue;
+            
+            if(strstr(filename2, ".c")){
+                strcpy(filename3, ansFile[j].dirName);
+                printf("../%s/%s/%s %d\n",pathname, stdFile[i].stdName, ansFile[j].dirName, i);
                 
-                strcpy(cmd, "gcc -o ");
-                // printf("%s %s\n", stdFile[i].stdName, stdFile[i].file[j].name);
-                // fd of c file in std dir
-                fd = stdFile[i].file[j].fd;
-                chdir(stdFile[i].stdName);
-                
-                strcpy(buf1, stdFile[i].file[j].name);
-
-                strcpy(buf2, buf1);
-                len = strlen(buf1);
-                // printf("%s %s %s %s\n",stdFile[i].file[j].name, buf1, buf2, cmd);
-                buf1[len-2] = 0;
-                strcpy(problemName, buf1);  // problemName
-                strcpy(fileerror, buf1);
-
-                strcat(buf1, ".stdexe ");
-                strcat(cmd, buf1);
-                strcat(cmd, buf2);
+                // gcc -o 11.stdexe ../../STD_DIR/20190001/11.c -lpthread
+                sprintf(cmd, "gcc -o %s.stdexe ../../%s/%s/%s.c -lpthread",filename3, pathname, filename1, filename3);
                 // printf("cmd = %s\n",cmd);
+                // chdir(filename2);
 
-                strcat(cmd, " -lpthread"); // -lpthread option
-                
-                
-                strcat(fileerror, "_error.txt");
-                
-                if((fd_err = open(fileerror, O_RDWR|O_CREAT|O_TRUNC, 0644)) < 0){
+                sprintf(fileerror, "%s_error.txt", filename3);
+                 if((fd_err = open(fileerror, O_RDWR|O_CREAT|O_TRUNC, 0644)) < 0){
                     fprintf(stderr,"open error for test.txt\n");
                     printf("%s\n",strerror(errno));
                     exit(1);
                 }
-                // 에러는 fd_err
                 dup2(fd_err, 2);
                 
                 // 컴파일 에러 처리해야함..
@@ -686,29 +682,22 @@ void runStdProgram(char *pathname){
                 
                 if(errorSize == 0){
                     char rmcmd[50];
-                    strcpy(rmcmd, "rm ");
-                    strcat(rmcmd, fileerror);
+                    sprintf(rmcmd, "rm %s",fileerror);
                     system(rmcmd);
                 }
 
                 alarm(TIMEOUT);
 
-                pthread_create(&tid, NULL, (void *)workerThread, (void *)problemName);
+                pthread_create(&tid, NULL, (void *)workerThread, (void *)filename3);
 
                 pthread_join(tid, NULL);
 
-                // 만약 에러가 나지 않으면 실행 후 결과를 파일에 출력
-                // printSTDProgram(problemName);
-                // 에러가 나면 에러파일 생성 후 결과를 파일에 출력
-
-                chdir("..");
             }
         }
+        chdir("..");
     }
     chdir("..");
     // printf("runSTDprogrma ends!\n");
-
-    // pthread_join(wtid, NULL);
 }
 void runAnsProgram(char *pathname){
     struct dirent *dentry1, *dentry2;
@@ -716,8 +705,7 @@ void runAnsProgram(char *pathname){
     char filename1[NAME_LEN], filename2[NAME_LEN];
     DIR *dirp1, *dirp2;
     
-    char curdir[200], buf[1024];
-    char cmd[50], buf1[50], buf2[50], ansName[50];
+    char cmd[2048], buf1[2048], buf2[2048];
     int fd,len,i=0;
 
 
@@ -728,124 +716,50 @@ void runAnsProgram(char *pathname){
     mkdir("ANS",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     // mkdir("ANS",775);
     chdir("ANS");
-    system("pwd");
 
-    printf("runAnsProgram\n---------------------\n");
-    system("pwd");
+    // printf("runAnsProgram\n---------------------\n");
+    // system("pwd");
     for(int i=0; i<problemNum; i++){
-        printf("../%s/%s/%s\n",pathname, ansFile[i].dirName, ansFile[i].name);
+        // printf("../%s/%s/%s %d\n",pathname, ansFile[i].dirName, ansFile[i].name, i);
     }
+    // printf("problemNum = %d\n",problemNum);
     for(int i=0; i<problemNum; i++){
         strcpy(filename1, ansFile[i].dirName);
+        // printf("make dir %s %d\n",filename1, i);
+        strcpy(filename2, ansFile[i].name);
         if(strcmp(filename1, "") == 0)
             continue;
-        printf("make dir %s %d\n",filename1, i);
-        
-        mkdir(filename1, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
-        // mkdir(filename1, 775); 
+        if(strstr(filename2, ".c")){
+            mkdir(filename1, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
+            // mkdir(filename1, 775); 
 
-        chdir(filename1);
-        system("pwd");
+            chdir(filename1);
+            // system("pwd");
+            
+            // printf("filename2 = %s\n",filename2);
+            if(strstr(filename2, ".c")){
+            // printf("runAnsProgram file in %s\n", filename2);
+
+            
+            sprintf(buf1, "%s.stdexe", ansFile[i].dirName); //11.stdexe
+
+            sprintf(buf2, "../../%s/%s/%s/%s",pathname, stdFile[i].stdName, ansFile[i].dirName,filename2);
+
+            sprintf(cmd, "gcc -o %s %s -lpthread",buf1, buf2);
+
+            // printf("cmd = %s\n",cmd);
+            
+            system(cmd);
+            // system("pwd");
+            
+            // 실행 후 결과를 파일에 출력
+            printANSProgram(ansFile[i].dirName);
+            // 에러가 나면 에러파일 생성 후 결과를 파일에 출력
+            }
+        }
         chdir("..");
     }
-
-    // if((dirp1 = opendir(pathname)) == NULL || chdir(pathname) == -1){
-    //     fprintf(stderr, "opendir: chdir error for %s\n",pathname);
-    //     printf("%s\n",strerror(errno));
-    //     exit(1);
-    // }
-
-    // while((dentry1 = readdir(dirp1)) != NULL){
-    //     if(dentry1->d_ino == 0)
-    //         continue;
-
-    //     memcpy(filename1, dentry1->d_name, NAME_LEN);
-    //     if(stat(filename1, &statbuf1) == -1){
-    //         fprintf(stderr, "stat error for %s\n", filename1);
-    //         printf("%s\n",strerror(errno));
-    //         exit(1);
-    //     }
-    //     // printf("runAnsProgram dir %s\n",filename1);
-    //     if(S_ISDIR(statbuf1.st_mode)){  // if directory
-    //         if(strncmp(".", filename1, 1) == 0 || strncmp("..", filename1, 2) == 0)
-    //             continue;
-    //         if((dirp2 = opendir(filename1)) == NULL){
-    //             fprintf(stderr, "opendir error for %s\n", filename2);
-    //             printf("%s\n",strerror(errno));
-    //             exit(1);
-    //         }
-    //         if(strcmp(filename1, "score_table.csv") == 0) continue;
-    //         chdir(filename1);    // change dir to child dir
-    //         // system("pwd");
-
-            
-    //         while((dentry2 = readdir(dirp2)) != NULL){
-    //             if(dentry1->d_ino == 0)
-    //                 continue;
-    //             memcpy(filename2, dentry2->d_name, NAME_LEN); 
-
-    //             if(strncmp(".", filename2, 1) == 0 || strncmp("..", filename2, 2) == 0)
-    //                 continue;
-    //             // printf("%s\n",filename2);
-                
-    //             if(stat(filename2, &statbuf2) == -1){
-    //                 fprintf(stderr, "stat error for %s\n", filename2);
-    //                 printf("%s\n",strerror(errno));
-    //                 exit(1);
-    //             }
-            
-    //             if(S_ISREG(statbuf2.st_mode)){
-    //                 printf("%s\n",filename2);
-    //                 if(strstr(filename2, ".c")){
-    //                     // printf("runAnsProgram file in %s\n", filename2);
-
-    //                     memset(buf1, 0, 50);
-    //                     memset(buf2, 0, 50);
-    //                     memset(cmd, 0, 50);
-                        
-    //                     strcpy(cmd, "gcc -o ");
-
-    //                     // fd of c file in ans dir
-    //                     fd = ansFile[i].fd;
-                        
-    //                     strcpy(buf1, filename2);
-
-    //                     strcpy(buf2, buf1);
-    //                     len = strlen(buf1);
-    //                     // printf("%s %s %s %s\n",filename2, buf1, buf2, cmd);
-    //                     buf1[len-2] = 0;
-                        
-    //                     strcpy(ansName, buf1);
-    //                     strcat(buf1, ".stdexe ");
-    //                     strcat(cmd, buf1);
-    //                     strcat(cmd, buf2);
-    //                     // printf("cmd = %s\n",cmd);
-                        
-    //                     strcat(cmd, " -lpthread");   // -lpthread opt
-    //                     system(cmd);
-    //                     // system("pwd");
-                        
-    //                     // 실행 후 결과를 파일에 출력
-    //                     printANSProgram(ansName);
-    //                     // 에러가 나면 에러파일 생성 후 결과를 파일에 출력
-    //                     i++;
-    //                 }
-    //             }
-    //         }
-    //         if(closedir(dirp2) == 0){
-    //             // printf("closedir dirp2\n");
-    //         }
-    //         chdir("..");
-    //         if(dentry1->d_ino == 0) continue;
-    //     }
-    // }
-    
-    // // problemNum = i;
-    // if(closedir(dirp1) == 0){
-    //     // printf("closedir dirp1\n");
-    // }
-    // chdir("..");
-   
+    chdir("..");
 }
 
 void printANSProgram(char *pathname){
@@ -899,18 +813,17 @@ void printSTDProgram(char *pathname){
     dup2(saved_stdout, 1);
 }
 
-int compareResult(char *pathname){
+int compareResult(char *pathname1, char *pathname2){
     //fd_ans를 읽어서 pathname(STD)와 비교한다.
-    struct dirent **namelist1, **namelist2;
-    struct stat statbuf1, statbuf2;
-    char ansName[50], fileName[50], buf1[2048], buf2[2048],ch;
+    char ansName[NAME_LEN], fileName1[NAME_LEN], fileName2[NAME_LEN];
+    // buf1[2048], buf2[2048];
+    char ch;
     int dircnt=0, filecnt=0, len;
     int fd_std[110];
     double probPoints[110];
 
-    int offsetLine[110]={0,}, lineCnt=0, offset=0;
+    int offset=0;
 
-    chdir(pathname);
     system("pwd");
 
 
@@ -934,66 +847,133 @@ int compareResult(char *pathname){
 
     printf("csvfd = %d\n",csvfd);
     
+    matchStdout(pathname1, pathname2);
+    
 
-    for(int i=0; i<studentNum; i++){
-        // STD[i] open
-        // if(strlen(stdFile[i].stdName) == 0 ) continue;
-        if( strcmp(stdFile[i].stdName, "") == 0 ) continue;
-        strcpy(ansName, stdFile[i].stdName);
-        chdir(ansName);
-        // system("pwd");
-        for(int j=0; j<problemNum; j++){
-        // printf("compareResult %s\n",stdFile[i].file[j].name);
-            if(strcmp(stdFile[i].file[j].name, "") == 0) continue;
+    // for(int i=0; i<studentNum; i++){
+    //     // STD[i] open
+    //     // if(strlen(stdFile[i].stdName) == 0 ) continue;
+    //     if( strcmp(stdFile[i].stdName, "") == 0 ) continue;
+    //     strcpy(ansName, stdFile[i].stdName);
+    //     chdir(ansName);
+    //     // system("pwd");
+    //     for(int j=0; j<problemNum; j++){
+    //     // printf("compareResult %s\n",stdFile[i].file[j].name);
+    //         if(strcmp(stdFile[i].file[j].name, "") == 0) continue;
             
-            strcpy(fileName, stdFile[i].file[j].name);
-            // printf("STD fileName = %s\n", fileName);
+    //         strcpy(fileName, stdFile[i].file[j].name);
+    //         // printf("STD fileName = %s\n", fileName);
 
-            for(int k=0; k<problemNum; k++){
-                if(strcmp(fileName, ansFile[k].name) == 0){
+    //         for(int k=0; k<problemNum; k++){
+    //             if(strcmp(fileName, ansFile[k].name) == 0){
 
-                    // 요기에 score_table.csv
-                    // read(fd_ans, buf2, 2048);
-                    // printf("%s\n",buf2);
-                    // printf("ANS = %10s, STD = %10s in  %10s\n",ansFile[k].name, fileName, stdFile[i].stdName);
-                    if(strstr(fileName, ".txt")){
-                        // matchBlank
-                    }
-                    else if(strstr(fileName, ".c")){
-                        // match .stdout
-                        // 요기에 ANS.stdout과 STD.stdout을 보내서 비교한다.
-                    }
+    //                 // 요기에 score_table.csv
+    //                 // read(fd_ans, buf2, 2048);
+    //                 // printf("%s\n",buf2);
+    //                 // printf("ANS = %10s, STD = %10s in  %10s\n",ansFile[k].name, fileName, stdFile[i].stdName);
+    //                 if(strstr(fileName, ".txt")){
+    //                     // matchBlank
+    //                 }
+    //                 else if(strstr(fileName, ".c")){
+    //                     // match .stdout
+    //                     // 요기에 ANS.stdout과 STD.stdout을 보내서 비교한다.
+    //                 }
                     
-                }
-            }
+    //             }
+    //         }
 
-            if((fd_std[j] = open(fileName, O_RDONLY)) < 0){
-                fprintf(stderr, "open error for %s\n",fileName);
+    //         if((fd_std[j] = open(fileName, O_RDONLY)) < 0){
+    //             fprintf(stderr, "open error for %s\n",fileName);
+    //             printf("%s\n",strerror(errno));
+    //             exit(1);
+    //         }
+    //         close(fd_std[j]);   // close fd_std[j]. 매우 중요함. Too many open files 를 보고 싶지 않다면 close 할 것.
+            
+            
+    //         // printf("before read\n");
+            
+    //         // while((len = read(fd_std[j], buf1, 2048)) > 0){
+    //         //     printf("%s\n",buf1);
+    //         // }
+
+    //         // ANS[j] open
+    //         // STD[i][j] open
+
+    //         // if .txt, matchBlank
+
+    //         // else .c, match .stdout
+
+    //         // if correct, find score_table.csv and score ++
+
+    //     }
+    //     chdir("..");
+    //     // close STD[i]
+    //     // printf("20190000 is finished... score is 46.8\n");
+    // }
+}
+
+int matchStdout(char *pathname1, char *pathname2){
+
+    char buf1[NAME_LEN], buf2[NAME_LEN], buf3[NAME_LEN];
+    char ansBlank[2048], ansProgram[2048], stdBlank[2048], stdProgram[2048];
+    int ansout_fd[110], stdout_fd[110];
+    // read ANS program
+    chdir("ANS");
+    for(int i=0; i<problemNum; i++){
+        strcpy(buf1, ansFile[i].dirName);
+        strcpy(buf2, ansFile[i].name);
+        if(strcmp(buf1, "") == 0)
+            continue;
+        if(strstr(buf2, ".c")){
+            sprintf(ansProgram, "ANS/%s/%s.stdout", buf1, buf1);
+            chdir(buf1);
+            // system("pwd");
+            // printf("ansProgram = %s\n",ansProgram);
+
+            strcpy(buf2, buf1);
+            strcat(buf2,".stdout");
+            if((ansout_fd[i] = open(buf2, O_RDWR)) < 0){
+                fprintf(stderr, "open error for %s\n",buf2);
                 printf("%s\n",strerror(errno));
                 exit(1);
             }
-            close(fd_std[j]);   // close fd_std[j]. 매우 중요함. Too many open files 를 보고 싶지 않다면 close 할 것.
+            // printf("fd = %d\n",ansout_fd[i]);
+            chdir("..");
+        }
+    }
+    chdir("..");
+
+    chdir("STD");
+    // read STD program
+    for(int i=0; i<studentNum; i++){
+        
+        strcpy(buf1, stdFile[i].stdName);
+        if(strcmp(buf1, "") == 0)
+            continue;
+        chdir(buf1);
+        system("pwd");
+        for(int j=0; j<problemNum; j++){
+            strcpy(buf3, ansFile[j].name);
+            strcpy(buf2, ansFile[j].dirName);
             
-            
-            // printf("before read\n");
-            
-            // while((len = read(fd_std[j], buf1, 2048)) > 0){
-            //     printf("%s\n",buf1);
-            // }
+            if(strcmp(buf2, "") == 0)
+                continue;
 
-            // ANS[j] open
-            // STD[i][j] open
+            if(strstr(buf3, ".c")){
+                strcat(buf2, ".stdout");
 
-            // if .txt, matchBlank
+                if((stdout_fd[j] = open(buf2, O_RDWR)) < 0){
+                    fprintf(stderr, "open error for %s\n",buf2);
+                    printf("%s\n",strerror(errno));
+                    exit(1);
+                }
+                printf("fd = %d\n",stdout_fd[j]);
 
-            // else .c, match .stdout
-
-            // if correct, find score_table.csv and score ++
-
+                //여기서 비교
+                compareFiles(ansout_fd[j], stdout_fd[j]);
+            }
         }
         chdir("..");
-        // close STD[i]
-        // printf("20190000 is finished... score is 46.8\n");
     }
 }
 
@@ -1037,7 +1017,7 @@ void *workerThread(void *arg){
     
     saved_stdout = dup(1);
 
-    pthread_cleanup_push(cleanup, (void *)saved_stdout);
+    pthread_cleanup_push(cleanup, (void *)&saved_stdout);
 
     // printf("workerThread tid = %u\n",pthread_self());
     
@@ -1059,7 +1039,8 @@ void sig_handler(int nSigNum){
     // tkill(tidNum, SIGTERM);
 }
 void cleanup(void *arg){
-    int saved_stdout = (int) arg;
+    int * intp = arg;
+    int saved_stdout = *intp;
     dup2(saved_stdout, 1);
     // printf("thread ends!\nalarm starts!\n");
     // alarm(3);
