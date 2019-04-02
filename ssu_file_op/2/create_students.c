23b8ef34ecc24ba9b7c5ad5dace7cbfa223116c3
bbvch13531 <bbvch13531@gmail.com>
2019-04-03 00:04:14 +0900

    DOC: Add project2
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ ssu_file_op/2/create_students.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//필요하면 header file 추가 가능
+
+//
+// argv[1]: 레코드 파일 students.dat에 저장되는 전체 레코드의 수
+//
+int main(int argc, char **argv)
+{
+
+    // 250바이트 학생 레코드를 students.dat 파일에 하나씩 저장하는 코드를 구현함
+    // 학생 레코드의 데이터는 임의로 생성해도 무방함
+    
+	return 0;
+}
