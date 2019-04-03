b74a9291efe9d78b4f330583c92d9e08c79701bd
bbvch13531 <bbvch13531@gmail.com>
2019-04-03 17:13:16 +0900

    FEAT: create_students
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_file_op/2/create_students.c
+++ ssu_file_op/2/create_students.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include "record.h"
 //필요하면 header file 추가 가능
 
@@ -12,14 +13,15 @@
 int main(int argc, char **argv){
     // 250 바이트 학생 레코드를 students.dat 파일에 하나씩 저장하는 코드를 구현함
     // 학생 레코드의 데이터는 임의로 생성해도 무방함
-    
+    time_t begin, end;
     FILE * fp;
     Student student = {"20142468", "kyungyoungheo", "Dongjak-gu", "soongsil", "computer", "Hello wo1112rld!"};
     int num;
 
     num = atoi(argv[1]);
     fp = fopen("students.dat", "w+");
-    printf("size of student = %ld\nsize of others = %ld",sizeof(Student), sizeof(student.others));
+    // printf("size of student = %ld\nsize of others = %ld",sizeof(Student), sizeof(student.others));
+
     for(int i=0; i<num; i++){
         // fwrite(student.id, sizeof(char), strlen(student.id)+1, fp);
         // fwrite(student.name, sizeof(char), strlen(student.name)+1, fp);
@@ -34,7 +36,7 @@ int main(int argc, char **argv){
         fwrite(student.univ, sizeof(char), 31, fp);
         fwrite(student.dept, sizeof(char), 41, fp);
         fwrite(student.others, sizeof(char), 71, fp);
-        fwrite("\n", sizeof(char), 1, fp);
+        // fwrite("\n", sizeof(char), 1, fp);
     }
     
     fclose(fp);
