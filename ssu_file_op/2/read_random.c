b2c15524a459b558a1b03ed3a4a4bc54d73b5c4c
bbvch13531 <bbvch13531@gmail.com>
2019-04-03 17:12:09 +0900

    FEAT: Add read_random
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_file_op/2/read_random.c
+++ ssu_file_op/2/read_random.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <time.h>
-#include <stdlib.h>
+#include "record.h"
 // 필요하면 header file 추가 가능
 
 
@@ -16,23 +18,69 @@ void swap(int *a, int *b);
 // argv[2]: 레코드 파일에 저장되어 있는 전체 레코드 수
 //
 int main(int argc, char **argv){
+	FILE *fp;
 	int *read_order_list;
 	int num_of_records;
+	int nextOffset;
+	char fname[100], buf[100];
+	char id[11], name[31], address[71], univ[31], dept[41], others[71];
+	Student *student_list;
+	Student student;
+
+	strcpy(fname, argv[1]);
+	num_of_records = atoi(argv[2]);
+	
+	fp = fopen(fname, "r+");
+	printf("%s %d\n", fname, num_of_records);
+	read_order_list = (int *)malloc(sizeof(int) * num_of_records);
 
 	// 이 함수를 실행하면 'read_order_list' 배열에 읽어야 할 레코드 번호들이 순서대로 나열되어 저장됨
 	// 'num_of_records'는 레코드 파일에 저장되어 있는 전체 레코드의 수를 의미함
 	GenRecordSequence(read_order_list, num_of_records);
-
-
+	// printf("Gen finished!\n");
 	// 'read_order_list'를 이용하여 표준 입력으로 받은 레코드 파일로부터 레코드를 random하게 읽어들이고,
     // 이때 걸리는 시간을 측정하는 코드 구현함
+	for(int i=0; i<num_of_records; i++){
+		printf("%d ",read_order_list[i]);
+	}
+	for(int i=0; i<num_of_records; i++){
+		nextOffset = read_order_list[i];
+		// printf("offset = %d \n", nextOffset);
+		fseek(fp, 256 * nextOffset, SEEK_SET);
+
+		fread(id, 1, 11, fp);
+		strcpy(student.id, id);
+		// printf("id = %s\n",id);
+
+		fread(name, 1, 31, fp);
+		strcpy(student.name, name);
+		// printf("name = %s\n",name);
+
+		fread(address, 1, 71, fp);
+		strcpy(student.address, address);
+		// printf("address = %s\n",address);
+
+		fread(univ, 1, 31, fp);
+		strcpy(student.univ, univ);
+		// printf("univ = %s\n",univ);
+
+		fread(dept, 1, 41, fp);
+		strcpy(student.dept, dept);
+		// printf("dept = %s\n",dept);
+
+		fread(others, 1, 71, fp);
+		strcpy(student.others, others);
+		// printf("others = %s\n",others);
+
+		printf("student = %s %s %s %s %s %s\n",student.id, student.name, student.address, student.univ, student.dept,student.others);
+	
+	}
 
 
 	return 0;
 }
 
-void GenRecordSequence(int *list, int n)
-{
+void GenRecordSequence(int *list, int n){
 	int i, j, k;
 
 	srand((unsigned int)time(0));
@@ -52,8 +100,7 @@ void GenRecordSequence(int *list, int n)
 	return;
 }
 
-void swap(int *a, int *b)
-{
+void swap(int *a, int *b){
 	int tmp;
 
 	tmp = *a;
