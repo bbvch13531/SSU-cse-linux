70cfa1f3ba8e44711d869a85a45e8ee727899849
bbvch13531 <bbvch13531@gmail.com>
2019-04-03 12:26:15 +0900

    FEAT: Add create
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_file_op/2/create_students.c
+++ ssu_file_op/2/create_students.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include "record.h"
 //필요하면 header file 추가 가능
 
@@ -11,9 +14,24 @@ int main(int argc, char **argv){
     // 학생 레코드의 데이터는 임의로 생성해도 무방함
     
     FILE * fp;
+    Student student = {"20142468", "kyungyoungheo", "Dongjak-gu", "soongsil", "computer", "Hello wo1112rld!"};
+    int num;
 
-    fp = fopen("students.dat", "r+");
+    num = atoi(argv[1]);
+    fp = fopen("students.dat", "w+");
+    printf("size of student = %ld\nsize of others = %ld",sizeof(Student), sizeof(student.others));
+    for(int i=0; i<num; i++){
+        fwrite(student.id, sizeof(char), strlen(student.id)+1, fp);
+        fwrite(student.name, sizeof(char), strlen(student.name)+1, fp);
+        fwrite(student.address, sizeof(char), strlen(student.address)+1, fp);
+        fwrite(student.univ, sizeof(char), strlen(student.univ)+1, fp);
+        fwrite(student.dept, sizeof(char), strlen(student.dept)+1, fp);
+        fwrite(student.others, sizeof(char), strlen(student.others)+1, fp);
+        
+        fwrite("\n", sizeof(char), 1, fp);
+    }
     
+    fclose(fp);
 
 	return 0;
 }
