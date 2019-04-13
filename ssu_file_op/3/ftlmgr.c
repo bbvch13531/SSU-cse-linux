0320181f14fb618af4819eec56c8df5e3267b11b
bbvch13531 <bbvch13531@gmail.com>
2019-04-13 23:55:43 +0900

    FEAT: Change to dd_read in-place update
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_file_op/3/ftlmgr.c
+++ ssu_file_op/3/ftlmgr.c
@@ -18,7 +18,7 @@ extern FILE *devicefp;
 // main()함수에서 반드시 먼저 호출이 되어야 한다.
 //
 
-int addressMappingTable[DATABLKS_PER_DEVICE];
+int addressMappingTable[BLOCKS_PER_DEVICE];
 int freeblock = BLOCKS_PER_DEVICE - 1;	// free block은 N-1번째로 초기화
 
 void ftl_open(){
@@ -30,7 +30,7 @@ void ftl_open(){
     // 하고, 그 이후 필요할 때마다 block을 하나씩 할당을 해 주면 됩니다. 어떤 순서대로 할당하는지는
     // 각자 알아서 판단하면 되는데, free block들을 어떻게 유지 관리할 지는 스스로 생각해 보기
     // 바랍니다.
-	for(int i=0; i<DATABLKS_PER_DEVICE; i++){
+	for(int i=0; i<BLOCKS_PER_DEVICE; i++){
 		addressMappingTable[i] = -1;
 	}
 	// BLOCK_PER_DEVICE 개의 table row 생성
@@ -97,12 +97,18 @@ void ftl_write(int lsn, char *sectorbuf){
 		}
 		else{	// in-place update
 			newpbn = freeblock * PAGES_PER_BLOCK;
-			addressMappingTable[lbn] = freeblock;
+			printf("pbn = %d\n",pbn);
 			for(int i=0; i<PAGES_PER_BLOCK; i++){
-				ftl_read(pbn + i, chkbuf);
-				if(chkbuf)
+				if(i == offset)
+					continue;
+				dd_read(pbn * PAGES_PER_BLOCK + i, chkbuf);
+				// spare를 확인하고 write를 결정.
 				dd_write(newpbn + i, chkbuf);
 			}
+
+			addressMappingTable[lbn] = freeblock;
+			addressMappingTable[pbn] = -1;
+			
 			// 의문 1. freeblock에 원래 block을 복사할 때 dd_read, dd_write해야하나?
 			// dd_read, dd_write하는 방향으로 구현함.
 			// 새로운 page는 다시 dd_Write. 흠... 이건 좀 아닌 것 같다. 질문하고 확인해볼 것.
@@ -118,7 +124,7 @@ void ftl_write(int lsn, char *sectorbuf){
 }
 
 void print(){
-	for(int i=0; i<DATABLKS_PER_DEVICE; i++){
+	for(int i=0; i<BLOCKS_PER_DEVICE; i++){
 		printf("%d %d\n", i, addressMappingTable[i]);
 	}
 }
