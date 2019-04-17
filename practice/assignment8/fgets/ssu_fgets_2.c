eeeb838123b8595dc34d14201e57a89200a247ab
bbvch13531 <bbvch13531@gmail.com>
2019-04-17 23:15:52 +0900

    FEAT: Add time calc
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/assignment8/fgets/ssu_fgets_2.c
+++ practice/assignment8/fgets/ssu_fgets_2.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "calTime.h"
 #define BUFFER_MAX 256
 
 int main(void){
+
+    struct timeval begin_t, end_t;
     char command[BUFFER_MAX];
     char *prompt = "Prompt>>";
 
+    gettimeofday(&begin_t, NULL);
     while(1){
         fputs(prompt, stdout);
 
@@ -16,5 +19,8 @@ int main(void){
     }
     fprintf(stdout, "Good but...\n");
     fflush(stdout);
+
+    gettimeofday(&end_t, NULL);
+    ssu_runtime(&begin_t, &end_t);
     exit(0);
 }
