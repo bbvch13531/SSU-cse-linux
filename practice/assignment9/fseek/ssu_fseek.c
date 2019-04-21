06fb627780840413b9cc49564fdde137a9355c6f
bbvch13531 <bbvch13531@gmail.com>
2019-04-21 18:45:00 +0900

    FEAT: Add fseek, ftell
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/assignment9/fseek/ssu_fseek.c
+++ practice/assignment9/fseek/ssu_fseek.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "calTime.h"
 
 int main(int argc, char **argv){
     char *fname = "ssu_test.txt";
     FILE *fp;
     long position;
     int character;
+    struct timeval begin_t, end_t;
+    
+    gettimeofday(&begin_t, NULL);
 
-    if((fp = fopen(fname, "r")) == NULL){
+    if((fp = fopen(fname, "r")) == NULL){   // 파일 열기
         fprintf(stderr, "fopen error for %s\n", fname);
         exit(1);
     }
 
     printf("Input number >>");
     scanf("%ld", &position);
-    fseek(fp, position - 1, SEEK_SET);
-    character = getc(fp);
-    printf("%ldth character => %c\n", position, character);
+    fseek(fp, position - 1, SEEK_SET);  // 파일 포인터 이동
+    character = getc(fp);   // 문자 읽어서 저장
+    printf("%ldth character => %c\n", position, character); // 문자 출력
+    gettimeofday(&end_t, NULL);
+    ssu_runtime(&begin_t, &end_t);
     
     exit(0);
 }
--- practice/assignment9/ftell/ssu_ftell.c
+++ practice/assignment9/ftell/ssu_ftell.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "calTime.h"
 
 int main(int argc, char **argv){
     char *fname = "ssu_test.txt";
     FILE *fp;
     long fsize;
-
+    struct timeval begin_t, end_t;
+    
+    gettimeofday(&begin_t, NULL);
+    
     if((fp = fopen(fname, "r")) == NULL){
         fprintf(stderr, "fopen error for %s\n", fname);
         exit(1);
     }
 
-    fseek(fp, 0, SEEK_END);
-    fsize = ftell(fp);
+    fseek(fp, 0, SEEK_END); // 파일의 맨 끝으로 이동
+    fsize = ftell(fp);  // 파일 포인터의 정보 가져오기
     printf("The size of <%s> is %ld bytes\n", fname, fsize);
+    gettimeofday(&end_t, NULL);
+    ssu_runtime(&begin_t, &end_t);
     
     exit(0);
 }
