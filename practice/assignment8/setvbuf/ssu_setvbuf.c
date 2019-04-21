73d0d7a2a6a23a66fced1ff81c9f2e996efbdf33
bbvch13531 <bbvch13531@gmail.com>
2019-04-21 18:43:58 +0900

    FEAT: Add setvbuf
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/assignment8/setvbuf/ssu_setvbuf.c
+++ practice/assignment8/setvbuf/ssu_setvbuf.c
@@ -13,20 +13,21 @@ int main(void){
     FILE *fp;
 
     gettimeofday(&begin_t, NULL);
-    if((fp = fopen(fname, "w")) == NULL){
+    if((fp = fopen(fname, "w")) == NULL){   //파일 열기
         fprintf(stderr, "fopen error for %s\n", fname);
         exit(1);
     }
 
-    ssu_setbuf(fp, buf);
-    fprintf(fp, "Hello, ");
+    ssu_setbuf(fp, buf);    // 파일의 버퍼를 buf로 설정
+    fprintf(fp, "Hello, "); 
     sleep(1);
     fprintf(fp, "UNIX!");
     sleep(1);
     fprintf(fp, "\n");
     sleep(1);
+    // 출력하고 1초씩 쉬기
 
-    ssu_setbuf(fp, NULL);
+    ssu_setbuf(fp, NULL);       // 파일의 버퍼를 NULL로 설정
     fprintf(fp, "HOW");
     sleep(1);
     fprintf(fp, "ARE");
@@ -35,6 +36,7 @@ int main(void){
     sleep(1);
     fprintf(fp, "\n");
     sleep(1);
+    // 출력하고 1초씩 쉬기
 
     gettimeofday(&end_t, NULL);
     ssu_runtime(&begin_t, &end_t);
@@ -46,7 +48,7 @@ void ssu_setbuf(FILE *fp, char *buf){
     int fd;
     int mode;
 
-    fd = fileno(fp);
+    fd = fileno(fp);    // 파일의 fileno를 fd에 저장
 
     if(isatty(fd)){
         mode = _IOLBF;
@@ -63,5 +65,5 @@ void ssu_setbuf(FILE *fp, char *buf){
         size = BUFFER_SIZE;
     }
 
-    setvbuf(fp, buf, mode, size);
+    setvbuf(fp, buf, mode, size);   // setvbuf를 설정한다.
 }
