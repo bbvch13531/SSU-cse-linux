aebc2e106ac90db654a4af22f90d9afc4e567296
bbvch13531 <bbvch13531@gmail.com>
2019-04-22 14:33:59 +0900

    FEAT: Add fork with buffer
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ practice/assignment10/fork2/ssu_fork_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "calTime.h"
+
+int main(int argc, char ** argv){
+    struct timeval begin_t, end_t;
+    pid_t pid;
+    char character, first, last;
+    long i;
+
+    gettimeofday(&begin_t, NULL);
+    
+    if((pid = fork()) > 0){
+        first = 'A';
+        last = 'Z';
+    }
+    else if(pid == 0){
+        first = 'a';
+        last = 'z';
+    }
+    else{
+        fprintf(stderr, "%s\n", argv[0]);
+        exit(1);
+    }
+
+    for(character = first; character <= last; character++){
+        for(i=0; i<=100000; i++)
+            ;
+        write(1, &character, 1);
+    }
+    printf("\n");
+    
+    gettimeofday(&end_t, NULL);
+    ssu_runtime(&begin_t, &end_t);
+    exit(0);
+}
