4f118d092ed06a489deaccdb4f9a036c6d75b636
bbvch13531 <bbvch13531@gmail.com>
2019-04-28 12:28:46 +0900

    FEAT: Finish wait func
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/assignment11/wait3/ssu_wait_3.c
+++ practice/assignment11/wait3/ssu_wait_3.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include "calTime.h"
 
 int main(void){
@@ -9,6 +10,16 @@ int main(void){
     
     gettimeofday(&begin_t, NULL);
     
+    if(fork() == 0){    // fork
+        execl("/bin/echo", "echo", "this is", "message one", (char *)0);
+    }
+    if(fork() == 0){    // fork
+        execl("/bin/echo", "echo", "this is", "message two", (char *)0);
+    }
+    printf("parent: waiting for children\n");
+    while(wait((int *)0) != -1); // 자식 프로세스가 종료될 때까지 기다림
+
+    printf("parent: all children terminated\n");
     gettimeofday(&end_t, NULL);
     ssu_runtime(&begin_t, &end_t);
     
