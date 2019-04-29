e7b596873a27b37ace25f80bc2d5d10028b9bfe4
bbvch13531 <bbvch13531@gmail.com>
2019-04-29 14:33:50 +0900

    FEAT: Add execv1 practice
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/assignment12/execv1/ssu_execv_1.c
+++ practice/assignment12/execv1/ssu_execv_1.c
@@ -4,7 +4,6 @@
 #include <errno.h>
 #include <sys/resource.h>
 #include <sys/wait.h>
-#include "calTime.h"
 
 double ssu_maketime(struct timeval *time);
 void term_stat(int stat);
@@ -15,6 +14,46 @@ int main(void){
     int status;
 
     if((pid = fork()) == 0){
-        char *args[]
+        char *args[] = {"find", "/", "-maxdepth", "4", "-name", "stdio.h", NULL};
+
+		if(execv("/usr/bin/find", args) < 0){
+			fprintf(stderr, "execv error\n");
+			exit(1);
+		}
+	}
+	if(wait3(&status, 0, &rusage) == pid)
+		ssu_print_child_info(status, &rusage);
+	else{
+		fprintf(stderr, "wait3 error\n");
+		exit(1);
+	}
+
     exit(0);
 }
+
+double ssu_maketime(struct timeval *time){
+	return ((double)time -> tx_sec + (double)time -> tv_usec/1000000.0);
+}
+
+void term_stat(int stat){
+	if(WIFEXITED(stat))
+		printf("normally terminated. exit status = %d\n", WEXITSTATUS(stat));
+	else if(WIFSIGNALED(stat))
+		printf("abnormal termination by signal %d. %s\n", WTERMSIG(stat),
+#idfef WCOREDUMP
+			WCOREDUMP(stat) ? "core dumped" : "no core"
+#else
+			NULL
+#endif
+			);
+	else if(WIFSTOPPED(stat))
+		printf("stopped by signal %d\n", WSTOPSIG(stat));
+}
+
+void ssu_print_child_info(int stat, struct rusage *rusage){
+	printf("Termination info follows\n");
+	term_stat(stat);
+	printf("user CPU time : %.2f(sec)\n", ssu_maketime(&rusage->ru_utime));
+	printf("system CPU time : %.2f(sec)\n", ssu_maketime(&rusage->ru_stime));
+}
+
