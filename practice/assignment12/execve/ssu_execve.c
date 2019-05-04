1790ab505912fc656d1023bfe699036fc6bbb73a
bbvch13531 <bbvch13531@gmail.com>
2019-05-05 00:46:07 +0900

    FEAT: Add execve
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ practice/assignment12/execve/ssu_execve.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv){
+	char *argv[] = {"ssu_execl_test_1", "param1", "param2", (char *)0};
+    char *env[] = {
+        "NAME = value",
+        "nextname = nextvalue",
+        "HOME = /home/kyungyoungheo",
+        (char *)0
+    };
+    
+    printf("this is the original program\n");
+    execve("./ssu_execl_test_1", argv, env);
+    printf("%s\n", "This line should never get printed\n");
+	exit(0);
+}
