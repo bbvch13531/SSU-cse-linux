a6bb8dcb57442e9a3727f58ebc28fce5d35c678d
bbvch13531 <bbvch13531@gmail.com>
2019-05-04 09:23:08 +0900

    FEAT: Add search_dir
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ midterm/example/3/search_dir.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void sear_dir(char *dir, struct dirent *dentry){
+	struct stat statbuf;
+	DIR *dirp;
+	char cwd[1024];
+
+	getcwd(cwd, 1024);
+
+	if(lstat(dir, &statbuf) < 0){
+		exit(1);
+	}
+
+	if(!S_ISDIR(statbuf.st_mode)){
+		printf("%s/%s\n", cwd, dentry->d_name);
+		return ;
+	}
+
+	if((dirp = opendir(dir)) == NULL){
+		exit(1);
+	}
+
+	chdir(dir);
+
+	while((dentry = readdir(dirp)) != NULL){
+		if(dentry->d_ino == 0)
+			continue;
+		if(strcmp(dentry->d_name, ".") && strcmp(dentry->d_name, "..")){
+			sear_dir(dentry->d_name, dentry);
+		}
+	}
+	chdir(cwd);
+}
+
+int main(int argc, char *argv[]){
+	struct dirent *dentry;
+	if(argc <2){
+		exit(1);
+	}
+	sear_dir(argv[1], dentry);
+}
