965ad2f0ce7c17e3e578b7a705aa14e04d309240
bbvch13531 <bbvch13531@gmail.com>
2019-05-05 15:32:06 +0900

    DOC: Add reports
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/assignment12/execve/ssu_execve.c
+++ practice/assignment12/execve/ssu_execve.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char ** argv){
+int main(void){
 	char *argv[] = {"ssu_execl_test_1", "param1", "param2", (char *)0};
     char *env[] = {
         "NAME = value",
