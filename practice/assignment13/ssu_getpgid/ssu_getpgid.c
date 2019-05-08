44fff3abfdd33852938d1b87ec056cdda2bc171c
bbvch13531 <bbvch13531@gmail.com>
2019-05-08 11:48:32 +0900

    FEAT: Add getpgig
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ practice/assignment13/ssu_getpgid/ssu_getpgid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void){
+	pid_t pgid;
+	pid_t pid;
+	
+	if(argc < 2){
+		fprintf(stderr, "usage: %s <file>\n", argv[0]);
+		exit(1);
+	}
+	pid = getpid();
+	pgid = getpgid(atoi(argv[1]);
+	printf("pid: %d, pgid: %d\n", pid, pgid);
+	exit(0);
+}
