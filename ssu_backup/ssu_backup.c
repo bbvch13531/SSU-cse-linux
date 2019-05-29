6770876a00fc5fc215d8d1ff1845d5e8371ebdcb
bbvch13531 <bbvch13531@gmail.com>
2019-05-29 22:19:33 +0900

    FEAT: Implement prompt
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_backup/ssu_backup.c
+++ ssu_backup/ssu_backup.c
@@ -9,6 +9,31 @@
 #include <sys/stat.h>
 
 #define BACKUP_DIR "BACKUP_DIR"
+
+/*
+// add 기능
+*/
+void add(char *pathname, int argc, char **argv);
+
+/*
+//  주어진 문자열이 몇개의 단어인지 계산하는 함수
+*/
+int count_words(char *string);
+
+/*
+//  주어진 문자열의 cmd를 리턴하는 함수
+//  return:
+//          add
+//          remove
+//          compare
+//          recover
+//          list
+//          ls
+//          vi
+//          vim
+//          exit
+*/
+void getcmd(char *string, char *cmd);
 /*
 //  사용법 출력하고 종료하는 함수
 */
@@ -16,15 +41,13 @@ void print_usage_and_exit(void);
 
 /*
 //  백업 디렉터리 생성하는 함수
-//  return:
-//          -1: lstat error
-//          
 */          
-int create_backup_dir(char *pathname);
+void create_backup_dir(char *pathname);
 
 char backup_pathname[256];
 int main(int argc, char **argv){
-    
+    int cnt;
+    char inputbuf[256], cmd[10];
     // 인자가 없는 경우
     if(argc == 1){
         create_backup_dir("./");
@@ -38,15 +61,48 @@ int main(int argc, char **argv){
         // usage 출력 후 종료
         print_usage_and_exit();
     }
+
+    while(1){
+        printf("20142468>");
+        gets(inputbuf);
+        printf("%s\n", inputbuf);
+        getcmd(inputbuf, cmd);
+        cnt = count_words(inputbuf);
+        printf("cmd = %s, cnt = %d\n",cmd, cnt);
+    }
     exit(0);
 }
 
-int create_backup_dir(char *pathname){
+void add(char *pathname, int argc, char **argv){
+
+}
+
+void getcmd(char *string, char *cmd){
+    int len = strlen(string);
+
+    for(int i=0; i<len; i++){
+        if(string[i] == ' '){
+            cmd[i] = '\0';
+            return;
+        }
+        cmd[i] = string[i];
+    }
+}
+
+int count_words(char *string){
+    int len = strlen(string);
+    int cnt = 0;
+    for(int i=0; i<len; i++){
+        if(string[i] == ' ')
+            cnt++;
+    }
+    return cnt + 1;
+}
+
+void create_backup_dir(char *pathname){
     struct dirent **dentry;
     struct stat statbuf;
-    DIR *dirp;
 
-    int dircnt;
     if(access(pathname, F_OK) != 0){
         fprintf(stderr, "Directory not exist\n");
         print_usage_and_exit();
