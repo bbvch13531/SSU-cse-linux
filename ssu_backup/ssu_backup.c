649cdef5df67cd455e898fb3aa67303cefe82767
bbvch13531 <bbvch13531@gmail.com>
2019-05-29 21:06:29 +0900

    FEAT: Add create_backup_dir func
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_backup/ssu_backup.c
+++ ssu_backup/ssu_backup.c
@@ -4,8 +4,66 @@
 #include <string.h>
 #include <getopt.h>
 #include <pthread.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/stat.h>
+
+/*
+//  사용법 출력하고 종료하는 함수
+*/
+void print_usage_and_exit(void);
+
+/*
+//  백업 디렉터리 생성하는 함수
+//  return:
+//          -1: lstat error
+//          
+*/          
+int create_backup_dir(char *pathname);
 
 int main(int argc, char **argv){
     
+    
+    exit(0);
+}
+
+int create_backup_dir(char *pathname){
+    struct dirent **dentry;
+    struct stat statbuf;
+    DIR *dirp;
+
+    int dircnt;
+    if(access(pathname, F_OK) != 0){
+        fprintf(stderr, "Directory not exist\n");
+        print_usage_and_exit();
+    }
+    if(access(pathname, R_OK) != 0 || access(pathname, W_OK) != 0 || access(pathname, X_OK) != 0){
+        fprintf(stderr, "Permission denied\n");
+        print_usage_and_exit();
+    }
+
+    if(!S_ISDIR(statbuf.st_mode)){
+        fprintf(stderr, "pathname is not directory\n");
+        print_usage_and_exit();
+    }
+
+    if(lstat(pathname, &statbuf) < 0){
+        fprintf(stderr, "lstat error\n");
+        print_usage_and_exit();
+    }
+
+    // 디렉터리 인자가 주어지지 않은 경우
+    if(strcmp(pathname, "") == 0){
+        mkdir("../BACKUP_DIR", 0644);
+        
+    }
+    else{
+       
+    }
+}
+
+void print_usage_and_exit(void){
+    // ./ssu_backup 잘못된 인자로 실행시 사용법 출력 후 종료
+    printf("Usage: ./ssu_backup [PATHNAME]\n");
     exit(0);
 }
