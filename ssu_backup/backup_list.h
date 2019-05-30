4961becba1fb8319eee306581252a2f818c8ef39
bbvch13531 <bbvch13531@gmail.com>
2019-05-31 03:10:45 +0900

    SAVE: Will fix list pointers
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_backup/backup_list.h
+++ ssu_backup/backup_list.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 typedef struct _backup_list{
     struct Backup_list *next;
     char pathname[256];
@@ -13,7 +14,9 @@ typedef struct _backup_list{
         // 2: saved;
 } Backup_list;
 
-int find_backup_list(char *pathname, Backup_list *list){
+int find_backup_list(char *pathname, Backup_list **head){
+    Backup_list *list = *head;
+
     while(list->next != NULL){
         if(strcmp(pathname, list->pathname) == 0)
             return 1;
@@ -22,16 +25,28 @@ int find_backup_list(char *pathname, Backup_list *list){
     return 0;
 }
 
-void append_backup_list(Backup_list *data, Backup_list *list){
+void append_backup_list(Backup_list data, Backup_list **head){
+    Backup_list *list = *head;
+    Backup_list *new_node = (Backup_list *)malloc(sizeof(Backup_list));
+    
+    new_node->next = data.next;
+    strcpy(new_node->pathname, data.pathname);
+    new_node->interval = data.interval;
+    strcpy(new_node->options, data.options);
+    new_node->time = data.time;
+    new_node->status = data.status;
+
     while(list->next != NULL){
         list = list->next;
     }
-    list -> next = data;
+    list->next = new_node;
 }
 
 // return -1 when error occurs;
 // return 1 for normal state
-int save_recent_list(int n, Backup_list *list){
+int save_recent_list(int n, Backup_list **head){
+    Backup_list *list = *head;
+
     for(int i=0; i<n-1; i++){
         if(list->next == NULL){
             return -1;
@@ -43,12 +58,17 @@ int save_recent_list(int n, Backup_list *list){
 }
 
 // search before remove from backup_list
-void remove_from_list(char *pathname, Backup_list *list){
-    Backup_list tmp;
+void remove_from_list(char *pathname, Backup_list **head){
+    Backup_list *list = *head;
 
-    while(list->next != NULL){
+    Backup_list *tmp;
+
+    while(1){
         if(strcmp(list->pathname, pathname) == 0){
-            
+            tmp->next = list->next;
         }
+        tmp = list;
+        list = list->next;
+        if(list == NULL)
     }
 }
