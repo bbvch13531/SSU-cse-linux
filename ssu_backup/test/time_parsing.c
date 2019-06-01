15723bf20049914ce551080e9312c66b6bbfdada
bbvch13531 <bbvch13531@gmail.com>
2019-06-01 13:25:03 +0900

    FEAT: Parse time to string
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- ssu_backup/test/time_parsing.c
+++ ssu_backup/test/time_parsing.c
@@ -8,6 +8,10 @@ int main(void){
     struct tm *cur_time;
     int year, mon, mday, hour, min, sec;
 
+    char pathname = "/home/projects/test1.txt";
+    char postfix[256];
+    char yymmdd[10], hhmmss[10];
+
     timer = time(NULL);
     cur_time = localtime(&timer);
 
@@ -21,6 +25,12 @@ int main(void){
     year += 1900;
     year %= 100;
     mon += 1;
+
+    sprintf(yymmdd, "%02d%02d%02d", year, mon, mday);
+    sprintf(hhmmss, "%02d%02d%02d", hour, min, sec);
+    sprintf(postfix, "%s%s", yymmdd, hhmmss);
+
+    printf("yymmss=%s, hhmmss=%s, postfix=%s\n",yymmdd, hhmmss, postfix);
     printf("year: %2d month: %02d day: %d hour: %d min: %d sec: %d\n", year, mon, mday, hour, min, sec);
     return 0;
     
