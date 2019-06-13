fc8992fa0e8f1c17d01118836ed76a13b6418118
bbvch13531 <bbvch13531@gmail.com>
2019-06-14 02:12:48 +0900

    FEAT: Add pipe func
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- practice/assignment22/ssu_pipe_2/ssu_pipe_2.c
+++ practice/assignment22/ssu_pipe_2/ssu_pipe_2.c
@@ -12,22 +12,22 @@ int main(void){
     int pid;
     int pipe_fd[2];
 
-    pipe(pipe_fd);
+    pipe(pipe_fd);  // 파이프 생성
     
     if((pid = fork()) < 0){
         fprintf(stderr, "fork error\n");
         exit(1);
     }
     else if(pid > 0){
-        printf("PARENT: writing to the pipe \n");
-        write(pipe_fd[1], "OSLAB", 6);
-        printf("PARENT: waiting \n");
+        printf(" PARENT: writing to the pipe \n");
+        write(pipe_fd[1], "OSLAB", 6);  // 부모 프로세스에서 파이프에 write
+        printf(" PARENT: waiting \n");
         wait(NULL);
     }
     else{
-        printf("CHILD: reading to the pipe \n");
-        read(pipe_fd[1], buf , 6);
-        printf("CHILD: read \"%s\" \n", buf);
+        printf(" CHILD: reading to the pipe \n");
+        read(pipe_fd[0], buf , 6);  // 자식 프로세스에서 파이프에 read
+        printf(" CHILD: read \" %s \" \n", buf);
         exit(0);
     }
     
--- practice/assignment22/ssu_pipe_3/ssu_pipe_3.c
+++ practice/assignment22/ssu_pipe_3/ssu_pipe_3.c
@@ -10,7 +10,7 @@ int main(void){
     int pipe_fd[2];
     int pid;
 
-    if(pipe(pipe_fd) < 0){
+    if(pipe(pipe_fd) < 0){  // 파이프 생성
         fprintf(stderr, "pipe error");
         exit(1);
     }
@@ -19,22 +19,22 @@ int main(void){
         fprintf(stderr, "fork error\n");
         exit(1);
     }
-    else if(pid == 0){
+    else if(pid == 0){  //자식 프로세스
         close(pipe_fd[0]);
 
         while(1){
             memset(buf, 0x00, BUFFER_SIZE);
             sprintf(buf, "Hello Mother Process. My name is %d\n", getpid());
-            write(pipe_fd[1], buf, strlen(buf));
+            write(pipe_fd[1], buf, strlen(buf));    // 파이프에 write
             sleep(1);
         }
     }
-    else{
+    else{   // 부모 프로세스
         close(pipe_fd[1]);
 
         while(1){
             memset(buf, 0x00, BUFFER_SIZE);
-            read(pipe_fd[0], buf, BUFFER_SIZE);
+            read(pipe_fd[0], buf, BUFFER_SIZE); // 파이프에 read
             fprintf(stderr, "%s", buf);
         }
     }
